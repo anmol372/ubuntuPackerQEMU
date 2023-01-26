a63c802a30d80f17cf301edc7cde440e57d6da71
anmol372 <anmrasto@cisco.com>
2023-01-26 14:15:46 -0800

    update helm values

--- packer-ubuntu-server-uefi/scripts/11_launch_chart.sh
+++ packer-ubuntu-server-uefi/scripts/11_launch_chart.sh
@@ -1,24 +1,27 @@
-#!/bin/env bash
+#!/bin/bash
 
 # Define the name of the Helm chart
 chart_name=$1
+echo $chart_name
 
 # Define the name of the release
 release_name=$2
+echo $release_name
 
 # Define images.tar
 images=$3
 
+args=$(echo $4 | sed -e 's/^"//' -e 's/"$//')
+
 # Load an image from a tgz file
 docker load -i $images
 
-#TODO: load helm template with correct values.yaml
-
 # Use the helm template command to save the chart to a file
-helm template $chart_name $release_name > chart.yaml
+helm template $release_name $chart_name $args> $release_name.yaml
 
 # Update the value of imagePullPolicy to IfNotPresent
-sed -i 's/imagePullPolicy:.*/imagePullPolicy: IfNotPresent/g' chart.yaml
+sed -i'' -e 's/imagePullPolicy:.*/imagePullPolicy: IfNotPresent/g' $release_name.yaml
+rm $release_name.yaml-e
 
 # Run the chart.yaml file
 helm install --name $release_name -f chart.yaml $chart_name
--- packer-ubuntu-server-uefi/scripts/1_setup_cko.sh
+++ packer-ubuntu-server-uefi/scripts/1_setup_cko.sh
@@ -2,12 +2,13 @@
 
 # Import all environment variables
 set -a
-source /etc/environment
+source /etc/environment #2>/dev/null
 
 
 # Create cert-manager
-source 11_launch_chart.sh /tmp/resources/cert-manager-v1.11.0.tgz cert-manager /tmp/resources/scripts/cko_resources/cert-manager_images.tgz
-# helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.10.0 --set installCRDs=true --wait
+#source 11_launch_chart.sh ../prepImgChart/cko_resources/cert-manager-v1.11.0.tgz cko-jetstack-cert-manager ../prepImgChart/cko_resources/cert-manager_images.tgz "--namespace cert-manager --create-namespace --version v1.10.0 --set installCRDs=true"
+source 11_launch_chart.sh /tmp/resources/cert-manager-v1.11.0.tgz cko-jetstack-cert-manager /tmp/resources/cert-manager_images.tgz "--namespace cert-manager --create-namespace --version v1.10.0 --set installCRDs=true"
+# helm install cert-manager jetstack/cert-manager "--namespace cert-manager --create-namespace --version v1.10.0 --set installCRDs=true --wait"
 
 # create ns
 kubectl create ns netop-manager
@@ -15,7 +16,8 @@ kubectl create ns netop-manager
 # create secrets
 source 12_create_secrets.sh
 
-
+#TODO: load helm template with correct values.yaml for netop-org-manager
 # Create cko
-source 11_launch_chart.sh /tmp/resources/netop-org-manager-0.9.0.tgz cert-manager /tmp/resources/scripts/cko_resources/netop-org-manager_images.tgz
-# helm install netop-org-manager cko/netop-org-manager -n netop-manager --create-namespace --version 0.9.0 -f my_values.yaml --wait
+#source 11_launch_chart.sh ../prepImgChart/cko_resources/netop-org-manager-0.9.0.tgz cko-netop-org-manager ../prepImgChart/cko_resources/netop-org-manager_images.tgz "--namespace netop-manager --create-namespace --version 0.9.0 -f my_values.yaml"
+source 11_launch_chart.sh /tmp/resources/netop-org-manager-0.9.0.tgz cko-netop-org-manager /tmp/resources/netop-org-manager_images.tgz "--namespace  netop-manager --create-namespace --version 0.9.0 -f my_values.yaml"
+# helm install netop-org-manager cko/netop-org-manager "-n netop-manager --create-namespace --version 0.9.0 -f my_values.yaml --wait"
