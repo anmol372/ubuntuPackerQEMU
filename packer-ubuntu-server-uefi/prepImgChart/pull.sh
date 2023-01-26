956fba23021efff3a658a170f238907440980223
anmol372 <anmrasto@cisco.com>
2023-01-25 17:16:39 -0800

    update

---
+++ packer-ubuntu-server-uefi/prepImgChart/pull.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Define variables for chart name and repo URL
+source $1
+
+# Add the repo to Helm
+helm repo add $repo_name $repo_url
+
+# Fetch the latest chart
+helm fetch $repo_name/$chart_name
+
+# Extract the container images from the chart
+chart_file=$(ls $chart_name-*tgz)
+tar -xvzf $chart_file
+
+# Create a tar file with all of the container images
+images=$(helm template $chart_name | grep "image:" | awk '{print $2}')
+for image in $images; do
+  echo "Adding $image to tar file"
+  image=$(echo $image | sed -e 's/^"//' -e 's/"$//')
+  docker pull $image
+  docker save $image | gzip > cko.tgz
+done
+
+
+#tar -czvf $output_dir/$chart_name"_images.tar.gz" $(docker images -q)
+
+# Move the chart
+mv cko.tgz $output_dir/$chart_name"_images.tgz"
+mv $chart_file $output_dir/$chart_file
+
+# Cleanup
+rm -rf $chart_name
+rm cko.tgz
+docker rmi $(docker images -q)
--- packer-ubuntu-server-uefi/prepImgChart/setup.sh
+++ packer-ubuntu-server-uefi/prepImgChart/setup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 # Run cert-manager-pull.sh script
-source scripts/cert-manager-pull.sh
+#chart_name="cert-manager"
+#repo_name="jetstack"
+#repo_url="https://charts.jetstack.io"
+#output_dir="cko_resources"
+#source prepImgChart/cert-manager-pull.sh "cert-manager" "jetstack" "https://charts.jetstack.io" "cko_resources"
+
+source pull.sh cert-manager-pull.sh
 
 # Run cko-pull.sh script
-source scripts/cko-pull.sh
+#chart_name="netop-org-manager"
+#repo_name="cko"
+#repo_url="https://noironetworks.github.io/netop-helm"
+#output_dir="cko_resources"
+#source prepImgChart/pull.sh "netop-org-manager" "cko" "https://noironetworks.github.io/netop-helm" "cko_resources"
+
+source pull.sh cko-pull.sh
